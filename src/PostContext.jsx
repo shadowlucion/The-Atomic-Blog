2080e4151424ffd583ccf5531a48a9aeffa0c9a8
Saurabh Mishra <saileshmishra11.sm@gmail.com>
2023-08-14 17:17:28 +0530

    The atomic blog app is ready to use.

---
+++ src/PostContext.jsx
@@ -0,0 +1,55 @@
+import { faker } from "@faker-js/faker";
+import { createContext, useContext, useState } from "react";
+
+// 1) Creating an Context
+const PostContext = createContext();
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+
+export function PostProvider({ children }) {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  return (
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onAddPost: handleAddPost,
+        onClearPosts: handleClearPosts,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  );
+}
+
+export function usePosts() {
+  const context = useContext(PostContext);
+  if (context === undefined) throw new Error("Context is used outside.");
+  return context;
+}
